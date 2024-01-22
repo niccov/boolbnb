f285b64ac60e4e1b4226fc379bc8c5660b64c5f9
Samuelec14 <samuconf14@gmail.com>
2024-01-22 17:15:48 +0100

    visits - migration done

--- database/migrations/2024_01_22_144055_create_visits_table.php
+++ database/migrations/2024_01_22_144055_create_visits_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('visits', function (Blueprint $table) {
             $table->id();
+            $table->string('ip');
+            $table->date('date');
+
             $table->timestamps();
         });
     }
