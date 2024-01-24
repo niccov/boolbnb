5c62a99ecbdc2b384ede900d2fa7a3acfe3180ed
LorisMarzocchi <ml77182a@gmail.com>
2024-01-24 16:14:06 +0100

    fix f.key

--- database/migrations/2024_01_22_144628_create_add_apartment_id_to_messages_table.php
+++ database/migrations/2024_01_22_144628_create_add_apartment_id_to_messages_table.php
@@ -13,20 +13,24 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('add_apartment_id_to_messages', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('apartment_id');
-            $table->unsignedBigInteger('message_id');
-            $table->timestamps();
+        Schema::table('apartments', function (Blueprint $table) {
+
+
+            $table->unsignedBigInteger('message_id')->nullable();
+
 
             // Definizione delle chiavi esterne
-            $table->foreign('apartment_id')->references('id')->on('apartments')->onDelete('cascade');
-            $table->foreign('message_id')->references('id')->on('messages')->onDelete('cascade');
+
+            $table->foreign('message_id')->references('id')->on('messages') ->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('add_apartment_id_to_messages');
+
+         Schema::table('apartments', function (Blueprint $table){
+            $table->dropForeign(['message_id']);
+            $table->dropColumn(['message_id']);
+        });
     }
 };
