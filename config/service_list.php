a70a7a48f8aee1e9321024ac5c573966aca4ab1e
DrKillex <lcollaro1994@gmail.com>
2024-01-31 10:25:33 +0100

    ServiceSeeder

---
+++ config/service_list.php
@@ -0,0 +1,125 @@
+<?php
+
+return [
+    [
+        'name' => 'swimming pool',
+        'icon_url' => 'fa-person-swimming',
+
+    ],
+    [
+        'name' => 'wi-fi',
+        'icon_url' => "fa-wifi",
+
+    ],
+    [
+        'name' => 'breakfast',
+        'icon_url' => "fa-mug-saucer",
+
+    ],
+    [
+        'name' => 'parking',
+        'icon_url' => "fa-square-parking",
+
+    ],
+    [
+        'name' => 'reception',
+        'icon_url' => "fa-bell-concierge",
+
+    ],
+    [
+        'name' => 'commuting',
+        'icon_url' => "fa-van-shuttle",
+
+    ],
+    [
+        'name' => 'info-point',
+        'icon_url' => 'fa-info',
+
+    ],
+    [
+        'name' => 'laundery',
+        'icon_url' => 'fa-jug-detergent',
+
+    ],
+    [
+        'name' => 'camping',
+        'icon_url' => 'fa-campground',
+
+    ],
+    [
+        'name' => 'pets',
+        'icon_url' => 'fa-paw',
+
+    ],
+    [
+        'name' => 'air-conditioning',
+        'icon_url' => 'fa-snowflake',
+
+    ],
+    [
+        'name' => 'restaurant',
+        'icon_url' => 'fa-utensils',
+
+    ],
+    [
+        'name' => 'room-service',
+        'icon_url' => 'fa-bell-concierge',
+
+    ],
+    [
+        'name' => 'baby-sitting',
+        'icon_url' => 'fa-baby',
+
+    ],
+    [
+        'name' => 'meeting-room',
+        'icon_url' => "fa-handshake",
+
+    ],
+    [
+        'name' => 'bike-rental',
+        'icon_url' => 'fa-bicycle',
+
+    ],
+    [
+        'name' => 'massages',
+        'icon_url' => 'fa-spa',
+
+    ],
+    [
+        'name' => 'tennis',
+        'icon_url' => 'fa-table-tennis-paddle-ball',
+
+    ],
+    [
+        'name' => 'basket',
+        'icon_url' => 'fa-basketball',
+
+    ],
+    [
+        'name' => 'car-rental',
+        'icon_url' => 'fa-car-on',
+
+    ],
+    [
+        'name' => 'excurions',
+        'icon_url' => 'fa-person-hiking',
+
+    ],
+    [
+        'name' => 'fitness',
+        'icon_url' => 'fa-dumbbell',
+
+    ],
+    [
+        'name' => 'multilingual-reception',
+        'icon_url' => 'fa-language',
+
+    ],
+    [
+        'name' => 'weddings',
+        'icon_url' => 'fa-children',
+
+    ],
+
+];
